f5f9f359d0ee90506006762368b9c58cfbc12397
Muhammad Owais <165272983+owaiskhan18@users.noreply.github.com>
2026-01-13 13:27:10 +0500

    Change CORS middleware to use defined origins

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # 👈 IMPORTANT
+    allow_origins=origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
